bb698f660fa6a8829b8fc59558663960eb292ed3
Philip Culverston <philip.culverston@protonmail.ch>
2020-08-22 20:06:31 +0100

    Add files via upload

---
+++ Gallery.py
@@ -0,0 +1,12 @@
+# Imports the unofficial Notion API
+from notion.client import NotionClient
+
+# Obtain the `token_v2` value by inspecting your browser cookies on a logged-in session on Notion.so
+client = NotionClient(token_v2="1e428df2815e00f2cc7bc6edeb32bf187b4f5feb6629f0281c1b25e1196a2b8fe7b746633e50caf041cc22e95af1360545b294c2a0073145d13d8108a7a73a96349ef71cf249ee2c605bf1cefbb3")
+
+# Replace this URL with the URL of the page you want to edit
+Miku = client.get_collection_view("https://www.notion.so/0ebd3315de5446589185448132251bcd?v=6bec01f0809041b99bf3b49a02a6edfc")
+
+# Imports an image of Miku from notion and opens it in Firefox.
+
+myrecord.get()
